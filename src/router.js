53265d36afce56e6687d0ab265f12816bc838a7a
Carlos Augusto <carlos.augustodev@outlook.com>
2023-06-12 00:06:14 -0300

    Create file server,router and app for architecture on project

---
+++ src/router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { route } = require("./app");
+const router = express.Router();
+
+//Route Test
+/*router.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});*/
+
+module.exports = router;
